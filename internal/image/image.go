a9cfe2a2ccfb05a1e446799b5f77dc532fefc3a0
Alex Hamlin <alex@alexhamlin.co>
2023-03-04 22:00:46 -0800

    Clean up printing of repository references

--- internal/image/image.go
+++ internal/image/image.go
@@ -1,5 +1,7 @@
 package image
 
+import "fmt"
+
 type Digest string
 
 type Registry string
@@ -9,7 +11,15 @@ type Repository struct {
 	Namespace string
 }
 
+func (r Repository) String() string {
+	return fmt.Sprintf("%s/%s", r.Registry, r.Namespace)
+}
+
 type Image struct {
 	Repository
 	Tag string
+}
+
+func (i Image) String() string {
+	return fmt.Sprintf("%s:%s", i.Repository, i.Tag)
 }
