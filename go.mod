e351bc40f4b7bfb04024c79031acf88af90ec993
Alex Hamlin <alex@alexhamlin.co>
2023-03-04 18:54:12 -0800

    Holy $#@! this code is garbage but it transfers blobs for real

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module go.alexhamlin.co/magic-mirror
 
 go 1.20
+
+require github.com/google/go-containerregistry v0.13.0
+
+require (
+	github.com/docker/cli v20.10.20+incompatible // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v20.10.20+incompatible // indirect
+	github.com/docker/docker-credential-helpers v0.7.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+)
--- internal/blobmirror/transfer.go
+++ internal/blobmirror/transfer.go
@@ -1,10 +1,15 @@
 package blobmirror
 
 import (
+	"context"
 	"fmt"
 	"log"
-	"math/rand"
-	"time"
+	"net/http"
+	"net/url"
+
+	"github.com/google/go-containerregistry/pkg/authn"
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/remote/transport"
 
 	"go.alexhamlin.co/magic-mirror/internal/image"
 )
@@ -16,7 +21,97 @@ func transfer(dgst image.Digest, from []image.Repository, to image.Repository) e
 
 	log.Printf("[blobmirror] Transferring %s from %s to %s", dgst, from[0], to)
 
-	duration := time.Duration(rand.Uint64() % uint64(5*time.Second))
-	time.Sleep(duration)
+	fromTransport, err := newTransport(from[0].Registry, transport.PullScope)
+	if err != nil {
+		return err
+	}
+	fromClient := &http.Client{Transport: fromTransport}
+
+	fromURL := from[0].Registry.BaseURL()
+	fromURL.Path = fmt.Sprintf("/v2/%s/blobs/%s", from[0].Path, dgst)
+	fromReq, err := http.NewRequest(http.MethodGet, fromURL.String(), nil)
+	if err != nil {
+		return err
+	}
+	fromResp, err := fromClient.Do(fromReq)
+	if err != nil {
+		return err
+	}
+	defer fromResp.Body.Close()
+	if err := transport.CheckError(fromResp, http.StatusOK); err != nil {
+		return err
+	}
+
+	toTransport, err := newTransport(to.Registry, transport.PushScope)
+	if err != nil {
+		return err
+	}
+	toClient := &http.Client{Transport: toTransport}
+
+	requestUploadURL := to.Registry.BaseURL()
+	requestUploadURL.Path = fmt.Sprintf("/v2/%s/blobs/uploads/", to.Path)
+	requestUploadReq, err := http.NewRequest(http.MethodPost, requestUploadURL.String(), nil)
+	if err != nil {
+		return err
+	}
+	requestUploadResp, err := toClient.Do(requestUploadReq)
+	if err != nil {
+		return err
+	}
+	defer requestUploadResp.Body.Close()
+	if err := transport.CheckError(requestUploadResp, http.StatusAccepted); err != nil {
+		return err
+	}
+
+	doUploadURL, err := requestUploadURL.Parse(requestUploadResp.Header.Get("Location"))
+	if err != nil {
+		return err
+	}
+	doUploadQuery, err := url.ParseQuery(doUploadURL.RawQuery)
+	if err != nil {
+		return err
+	}
+	doUploadQuery.Add("digest", string(dgst))
+	doUploadURL.RawQuery = doUploadQuery.Encode()
+	doUploadReq, err := http.NewRequest(http.MethodPut, doUploadURL.String(), fromResp.Body)
+	if err != nil {
+		return err
+	}
+	doUploadReq.Header.Set("Content-Type", "application/octet-stream")
+	doUploadReq.Header.Set("Content-Length", fromResp.Header.Get("Content-Length"))
+	doUploadResp, err := toClient.Do(doUploadReq)
+	if err != nil {
+		return err
+	}
+	defer doUploadResp.Body.Close()
+	if err := transport.CheckError(doUploadResp, http.StatusCreated); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func newTransport(registry image.Registry, scopes ...string) (http.RoundTripper, error) {
+	gRegistry, err := name.NewRegistry(string(registry))
+	if err != nil {
+		return nil, err
+	}
+
+	authenticator, err := authn.DefaultKeychain.Resolve(gRegistry)
+	if err != nil {
+		authenticator = authn.Anonymous
+	}
+
+	imgScopes := make([]string, len(scopes))
+	for i, scope := range scopes {
+		imgScopes[i] = gRegistry.Scope(scope)
+	}
+
+	return transport.NewWithContext(
+		context.Background(),
+		gRegistry,
+		authenticator,
+		http.DefaultTransport,
+		imgScopes,
+	)
 }
