a1b2bbf96e2a7d2d58e09a4e694d2a119ced19e2
Alex Hamlin <alex@alexhamlin.co>
2023-03-06 23:34:46 -0800

    Refactor unit tests for work.Queue
    In particular, move the runtime progress thing to a function.

--- internal/work/queue_test.go
+++ internal/work/queue_test.go
@@ -87,20 +87,9 @@ func TestQueueConcurrencyLimit(t *testing.T) {
 	}
 	tasks := q.GetOrSubmitAll(values...)
 
-	// Try to force the Go runtime to make progress on at least the number of
-	// tasks that will result in a breach. There's some inherent non-determinism
-	// in this strategy that could reveal itself if some other test leaks runnable
-	// goroutines, but as of this writing it appears to work just as well as
-	// time.Sleep calls that are about an order of magnitude more expensive.
-	gomaxprocs := runtime.GOMAXPROCS(1)
-	for i := 0; i < workerCount+1; i++ {
-		runtime.Gosched()
-	}
-	runtime.GOMAXPROCS(gomaxprocs)
+	forceRuntimeProgress(workerCount + 1)
 	close(unblock)
-
 	assertTaskSucceedsWithin[[]int](t, 2*time.Second, tasks, values)
-
 	if breached.Load() {
 		t.Errorf("queue breached limit of %d workers in flight", workerCount)
 	}
@@ -117,8 +106,8 @@ func TestQueueDetach(t *testing.T) {
 		countDetached      atomic.Int32
 		unblockReattach    = make(chan struct{})
 		reattachedInflight atomic.Int32
-		unblockReturn      = make(chan struct{})
 		breachedReattach   atomic.Bool
+		unblockReturn      = make(chan struct{})
 	)
 	q := NewQueue(workerCount, func(ctx context.Context, x int) (int, error) {
 		if err := Detach(ctx); err != nil {
@@ -128,7 +117,6 @@ func TestQueueDetach(t *testing.T) {
 		<-awaitDetached
 
 		<-unblockReattach
-
 		if err := Reattach(ctx); err != nil {
 			panic(err)
 		}
@@ -161,16 +149,9 @@ func TestQueueDetach(t *testing.T) {
 		t.Fatalf("not all workers successfully detached: %d running, want %d", count, submitCount)
 	}
 
-	// Try to force progress on tasks. See above for details.
-	// TODO: Consider refactoring this into a function.
 	close(unblockReattach)
-	gomaxprocs := runtime.GOMAXPROCS(1)
-	for i := 0; i < workerCount+1; i++ {
-		runtime.Gosched()
-	}
-	runtime.GOMAXPROCS(gomaxprocs)
+	forceRuntimeProgress(workerCount + 1)
 	close(unblockReturn)
-
 	assertTaskSucceedsWithin[[]int](t, 2*time.Second, tasks, want)
 
 	if breachedReattach.Load() {
@@ -217,5 +198,24 @@ func assertTaskBlocked[T any](t *testing.T, task *Task[T]) {
 		// TODO: Can we do this without touching Task internals?
 		t.Errorf("task was not blocked")
 	default:
+	}
+}
+
+// forceRuntimeProgress attempts to force the Go runtime to make progress on at
+// least n other goroutines before resuming the current one.
+//
+// This function is intended to help reveal issues with concurrency limits by
+// forcing the goroutines subject to those limits to hit some kind of blocking
+// condition simultaneously. When the only other live goroutines in a test are
+// those spawned by the test itself, this approach appears in practice to work
+// just as well as other strategies that may be at least an order of magnitude
+// more expensive (e.g. time.Sleep calls). However, there is some inherent
+// non-determinism in this strategy that could reveal itself if some other test
+// leaks runnable goroutines.
+func forceRuntimeProgress(n int) {
+	gomaxprocs := runtime.GOMAXPROCS(1)
+	defer runtime.GOMAXPROCS(gomaxprocs)
+	for i := 0; i < n; i++ {
+		runtime.Gosched()
 	}
 }
