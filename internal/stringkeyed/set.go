3f34db1a15ebd16db167142f4dfccf828ee9801f
Alex Hamlin <alex@alexhamlin.co>
2023-09-30 16:14:03 -0700

    Special-case the set of the empty string
    With this change, most sets will now have the same internal
    representation as before, without the extra byte at the front.
    Naturally, the special case works since the internal representation
    doesn't normally permit concatenating two copies of the empty string.

--- internal/stringkeyed/set.go
+++ internal/stringkeyed/set.go
@@ -23,13 +23,16 @@ var _ assertComparable[Set]
 // a valid and empty set.
 type Set struct {
 	// The internal representation of a Set is formed by sorting its raw elements,
-	// encoding each one, prepending the byte 0x1F (the ASCII Unit Separator
-	// character) to each one, and concatenating the result.
+	// encoding each one, and concatenating them with the byte 0x1F (the ASCII
+	// Unit Separator character) as a separator.
 	//
 	// The per-element encoding has two forms. If the encoded element begins with
 	// the byte 0x0E (the ASCII Shift Out character), the remaining bytes of the
 	// encoded element are an Ascii85 encoding of the original raw element.
 	// Otherwise, the encoded element is equivalent to the original raw element.
+	//
+	// As a special case, the set containing only the empty string is represented
+	// by the string containing only the separator byte.
 	//
 	// The representation of a particular set of elements is not guaranteed to
 	// remain stable over time. This internal representation must not be stored or
@@ -44,9 +47,11 @@ func (s *Set) Add(elems ...string) {
 	all := append(s.ToSlice(), elems...)
 	slices.Sort(all)
 	all = slices.Compact(all)
-	if len(all) > 0 {
+	if len(all) == 1 && all[0] == "" {
+		s.joined = unitSeparator
+	} else {
 		encodeAll(all)
-		s.joined = unitSeparator + strings.Join(all, unitSeparator)
+		s.joined = strings.Join(all, unitSeparator)
 	}
 }
 
@@ -54,23 +59,28 @@ func (s *Set) Add(elems ...string) {
 // contains. It is more efficient than computing the length of the slice
 // returned by ToSlice.
 func (s Set) Cardinality() int {
-	return strings.Count(s.joined, unitSeparator)
+	switch s.joined {
+	case "":
+		return 0
+	case unitSeparator:
+		return 1
+	default:
+		return 1 + strings.Count(s.joined, unitSeparator)
+	}
 }
 
 // ToSlice returns a sorted slice of the elements in s.
 func (s Set) ToSlice() []string {
-	if len(s.joined) == 0 {
+	switch s.joined {
+	case "":
 		return nil
+	case unitSeparator:
+		return []string{""}
+	default:
+		all := strings.Split(s.joined, unitSeparator)
+		decodeAll(all)
+		return all
 	}
-
-	all := strings.Split(s.joined, unitSeparator)
-	if len(all) < 1 || all[0] != "" {
-		panic("invalid stringkeyed.Set encoding")
-	}
-
-	all = all[1:]
-	decodeAll(all)
-	return all
 }
 
 func (s Set) MarshalJSON() ([]byte, error) {
