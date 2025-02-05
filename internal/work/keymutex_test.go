15e52a4c7ab618f5e51ad4cf300f7c7b490a0e12
Alex Hamlin <alex@alexhamlin.co>
2025-02-04 20:57:22 -0800

    Make normal KeyMutex tests look like synctest variants
    At this point, all of the relevant tests should look closer to 1:1.

--- internal/work/keymutex_test.go
+++ internal/work/keymutex_test.go
@@ -6,35 +6,29 @@ import (
 )
 
 func TestKeyMutexBasic(t *testing.T) {
-	const (
-		nKeys    = 3
-		nWorkers = nKeys * 2
-	)
-	var (
-		km          KeyMutex[int]
-		locked      [nKeys]atomic.Int32
-		hasStarted  = make(chan struct{})
-		canReturn   = make(chan struct{})
-		hasFinished = make(chan struct{}, nWorkers)
-	)
-	for i := 0; i < nWorkers; i++ {
-		key := i / 2
-		go func() {
-			defer func() { hasFinished <- struct{}{} }()
-			hasStarted <- struct{}{}
+	const keyCount = 3
+	const workerCount = 2 * keyCount
 
+	var (
+		km      KeyMutex[int]
+		locked  [keyCount]atomic.Int32
+		unblock = make(chan struct{})
+	)
+	for i := 0; i < workerCount; i++ {
+		key := i / 2
+		async(t, func() {
 			km.Lock(key)
 			defer km.Unlock(key)
 
 			locked[key].Add(1)
 			defer locked[key].Add(-1)
-			<-canReturn
-		}()
+
+			<-unblock
+		})
 	}
 
-	// Wait for every goroutine to be running, then force them all forward and
-	// check for limit breaches.
-	assertReceiveCount(t, nWorkers, hasStarted)
+	// Try to wait for every goroutine to be blocked, then check for limit
+	// breaches.
 	forceRuntimeProgress()
 	for i := range locked {
 		if count := locked[i].Load(); count > 1 {
@@ -42,55 +36,50 @@ func TestKeyMutexBasic(t *testing.T) {
 		}
 	}
 
-	// Wait for the workers to finish.
-	close(canReturn)
-	assertReceiveCount(t, nWorkers, hasFinished)
+	// Let all of the workers finish.
+	close(unblock)
 }
 
 func TestKeyMutexDetachReattach(t *testing.T) {
 	var (
-		km      KeyMutex[NoValue]
-		workers [1]func(*QueueHandle)
+		km       KeyMutex[NoValue]
+		started0 = make(chan struct{})
+		locked0  = make(chan struct{})
+		unblock0 = make(chan struct{})
 	)
-
-	var (
-		w0HasStarted = make(chan struct{})
-		w0HasLocked  = make(chan struct{})
-		w0CanUnlock  = make(chan struct{})
-	)
-	workers[0] = func(qh *QueueHandle) {
-		close(w0HasStarted)
-		km.LockDetached(qh, NoValue{})
-		close(w0HasLocked)
-		<-w0CanUnlock
-		km.Unlock(NoValue{})
-	}
-
 	q := NewQueue(1, func(qh *QueueHandle, x int) (int, error) {
-		if x >= 0 && x < len(workers) {
-			workers[x](qh)
+		if x == 0 {
+			close(started0)
+			km.LockDetached(qh, NoValue{})
+			close(locked0)
+			<-unblock0
+			km.Unlock(NoValue{})
 		}
 		return x, nil
 	})
 
-	// Start the handler for 0, but force it to detach by holding the lock first.
+	// Take the lock.
 	km.Lock(NoValue{})
-	async(t, func() { q.Get(0) })
-	<-w0HasStarted
 
-	// Ensure that unrelated handlers can proceed while handler 0 awaits the lock.
+	// Start the handler for 0, which will have to detach since we're holding the
+	// lock.
+	async(t, func() { q.Get(0) })
+	assertReceiveCount(t, 1, started0)
+
+	// Ensure that unrelated handlers can, in fact, proceed.
 	assertIdentityResults(t, q, 1)
 
-	// Allow handler 0 to obtain the lock.
+	// Release the lock so handler 0 can obtain it.
 	km.Unlock(NoValue{})
-	<-w0HasLocked
+	assertReceiveCount(t, 1, locked0)
 
-	// Ensure that unrelated handlers are blocked.
+	// Start another handler, and ensure it really is blocked behind handler 0.
 	assertKeyBlocked(t, q, 2)
 
-	// Allow both handlers to finish.
-	close(w0CanUnlock)
-	assertIdentityResults(t, q, 0, 2)
+	// Allow all of the handlers to finish.
+	close(unblock0)
+	keys := []int{0, 1, 2}
+	assertIdentityResults(t, q, keys...)
 }
 
 func TestKeyMutexDoubleUnlock(t *testing.T) {
