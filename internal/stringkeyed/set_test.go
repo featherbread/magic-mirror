6fe61304b1767e8bdd481dcf10815679f62e1e94
Alex Hamlin <alex@alexhamlin.co>
2025-06-15 17:33:06 -0700

    Go back to a single set of JSON tests
    These are basically the same tests now; let's only focus on the
    different implementations.

--- internal/stringkeyed/set_test.go
+++ internal/stringkeyed/set_test.go
@@ -1,6 +1,7 @@
 package stringkeyed
 
 import (
+	"encoding/json"
 	"math/rand/v2"
 	"slices"
 	"testing"
@@ -98,6 +99,34 @@ func TestSetIterateBreak(t *testing.T) {
 	s := SetOf("one", "two", "three")
 	for range s.All() {
 		break // Should not panic.
+	}
+}
+
+func TestSetMarshalJSON(t *testing.T) {
+	s := SetOf("one", "two", "three")
+	want := []byte(`["one","three","two"]`)
+	got, err := json.Marshal(s)
+	assert.NoError(t, err)
+	assert.Equal(t, want, got)
+}
+
+func TestSetUnmarshalJSON(t *testing.T) {
+	var s Set
+	if err := json.Unmarshal([]byte(`["one","two","three"]`), &s); err != nil {
+		t.Fatalf("error unmarshaling valid Set from JSON: %v", err)
+	}
+	want := []string{"one", "three", "two"}
+	got := s.ToSlice()
+	assert.Equal(t, want, got)
+}
+
+func TestSetUnmarshalJSONInvalid(t *testing.T) {
+	var s Set
+	err := json.Unmarshal([]byte(`["one","two","one"]`), &s)
+	if err == nil {
+		t.Error("successfully unmarshaled an invalid Set from JSON")
+	} else {
+		t.Logf("unmarshal error was: %v", err)
 	}
 }
 
