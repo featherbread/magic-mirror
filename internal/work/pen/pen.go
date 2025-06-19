4fe5e97855571774fbf8bfc43ea683a5245d0d81
Alex Hamlin <alex@alexhamlin.co>
2025-06-19 00:13:47 -0700

    Polish pen.Result docs

--- internal/work/pen/pen.go
+++ internal/work/pen/pen.go
@@ -17,7 +17,7 @@ func Do[T any](fn func() (T, error)) (r Result[T]) {
 }
 
 // DoOrExit runs fn in the current goroutine and captures a return or panic.
-// Unlike [Do], it propagates [runtime.Goexit] from fn rather than returning.
+// Unlike [Do], it propagates [runtime.Goexit] without returning.
 func DoOrExit[T any](fn func() (T, error)) (r Result[T]) {
 	r.started = true
 	func() {
@@ -60,7 +60,7 @@ func Return[T any](value T, err error) Result[T] {
 }
 
 // Result captures the exit behavior of an isolated function. The zero value
-// behaves as if capturing the return of a zero value and nil error.
+// behaves as if capturing the return of a zero T and nil error.
 type Result[T any] struct {
 	started   bool
 	returned  bool
@@ -70,7 +70,7 @@ type Result[T any] struct {
 	panicval  any
 }
 
-// Unwrap propagates the result of an isolated function to the current goroutine:
+// Unwrap propagates the captured result to the current goroutine:
 // returning its values, panicking, or calling [runtime.Goexit].
 func (r Result[T]) Unwrap() (T, error) {
 	switch {
@@ -84,12 +84,12 @@ func (r Result[T]) Unwrap() (T, error) {
 	}
 }
 
-// Goexited returns true if this result captures a [runtime.Goexit] call.
+// Goexited is true if this result captures a [runtime.Goexit] call.
 func (r Result[T]) Goexited() bool {
 	return r.started && !r.returned && !r.recovered
 }
 
-// Panicked returns true if this result captures a panic.
+// Panicked is true if this result captures a panic.
 func (r Result[T]) Panicked() bool {
 	return r.started && !r.returned && r.recovered
 }
@@ -99,9 +99,11 @@ func (r Result[T]) Returned() bool {
 	return !r.started || r.returned
 }
 
-// Recovered returns any panic value captured by this result. This value may be
-// nil if the result does not capture a panic, or if the panicnil=1 GODEBUG is
-// enabled. [Panicked] can distinguish whether a nil panic actually occurred.
+// Recovered returns any panic value captured by this result.
+//
+// If the GODEBUG panicnil=1 setting is enabled, a nil Recovered value may
+// represent a true "panic(nil)". [Result.Panicked] distinguishes nil panics
+// from non-panic results.
 func (r Result[T]) Recovered() any {
 	return r.panicval
 }
