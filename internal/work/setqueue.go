6f36e132ffcf936b8868a35fb8650f80e21d8df1
Alex Hamlin <alex@alexhamlin.co>
2025-06-18 19:21:17 -0700

    Simplify SetQueue documentation

--- internal/work/setqueue.go
+++ internal/work/setqueue.go
@@ -3,13 +3,13 @@ package work
 // SetHandler is a type for a [SetQueue]'s handler function.
 type SetHandler[K comparable] = func(*QueueHandle, K) error
 
-// SetQueue represents a [Queue] whose handlers return no meaningful value.
+// SetQueue wraps a [Queue] whose handlers return no meaningful value with a
+// simplified error-only API.
 type SetQueue[K comparable] struct {
 	*Queue[K, struct{}]
 }
 
-// NewSetQueue creates a [SetQueue] in a manner equivalent to how [NewQueue]
-// creates a [Queue].
+// NewSetQueue is analogous to [NewQueue].
 func NewSetQueue[K comparable](concurrency int, handle SetHandler[K]) SetQueue[K] {
 	return SetQueue[K]{
 		Queue: NewQueue(concurrency, func(qh *QueueHandle, key K) (_ struct{}, err error) {
@@ -19,14 +19,13 @@ func NewSetQueue[K comparable](concurrency int, handle SetHandler[K]) SetQueue[K
 	}
 }
 
-// Get is analogous to [Queue.Get], but does not return a meaningful value.
+// Get is analogous to [Queue.Get].
 func (q SetQueue[K]) Get(key K) error {
 	_, err := q.Queue.Get(key)
 	return err
 }
 
-// Collect is analogous to [Queue.Collect], but does not return meaningful
-// values.
+// Collect is analogous to [Queue.Collect].
 func (q SetQueue[K]) Collect(keys ...K) error {
 	_, err := q.Queue.Collect(keys...)
 	return err
