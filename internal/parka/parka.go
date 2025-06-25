8583e5dedc5dc73b39a5d6941b332037bc184533
Alex Hamlin <alex@alexhamlin.co>
2025-06-24 22:11:32 -0700

    Light doc editing

--- internal/parka/parka.go
+++ internal/parka/parka.go
@@ -1,14 +1,13 @@
 /*
 Package parka provides managed concurrency abstractions for keyable work.
 
-Parka's core abstraction, the [Map], composes duplicate call suppression,
-caching, and bounded concurrency into a single "lazy map" concept, eliminating
-traditional boilerplate for distributing work across goroutines. Maps provide
-substantial dynamic control over concurrency limits and task prioritization,
-including at the level of individual running handlers.
+Parka's [Map] composes duplicate call suppression, caching, and bounded
+concurrency into a single mechanism for distributing work across goroutines.
+Maps provide substantial dynamic control over concurrency limits and task
+prioritization, including at the level of individual running handlers.
 
-This version of Parka is best suited for work that does not require task
-cancellation or cache eviction, such as a main package that runs a single
-batch-type workflow and exits immediately on success or error.
+This version of Parka is suited for work that does not require task cancellation
+or cache eviction, such as a main package that runs a single batch-type workflow
+and exits immediately on success or error.
 */
 package parka
