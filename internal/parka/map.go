736f89bab2d21fea5d0dba653db6943efecf620d
Alex Hamlin <alex@alexhamlin.co>
2025-06-24 18:20:42 -0700

    Persist an initial chain of thought on Reattach soundness
    I think this is going down the wrong path, but I have a better idea of
    where to go from here.

--- internal/parka/map.go
+++ internal/parka/map.go
@@ -433,12 +433,32 @@ func (h *Handle) Reattach() {
 	if shouldReattach {
 		h.reattach()
 	}
-	// TODO: Out of multiple concurrent Reattach calls, only one will wait for the
-	// reattach to finish. Does that matter? I mean, the _point_ of Reattach is
-	// that you want to lose some concurrency. I think I'll just get creative with
-	// the documentation on this one. It's still better than the old racy version.
-	// TODO TODO: No, I think there may be a real problem here, but I'm too tired
-	// to think through it.
+	// TODO: Reattach has never been sound against the handler invoking it in a
+	// goroutine other than its own. When this happens, we may provide a work
+	// grant to a goroutine that's not prepared to handle its invariants, and
+	// permanently burn the concurrency it represents.
+	//
+	// I think we can solve this if the main work loop tracks whether the handler
+	// has _ever_ reattached, and if so spawns a fresh `go work(nil)` to replace
+	// itself. There are two cases to consider:
+	//
+	//   1. This goroutine waited properly for the Reattach, so the main work loop
+	//      actually holds a work grant. Spawning the new worker is just as valid
+	//      as in the typical Goexit case.
+	//
+	//   2. This goroutine spawned a _concurrent_ Reattach. Another goroutine may
+	//      hold the work grant destined for that worker, and when the work grant
+	//      makes it to that worker it will be burned.
+	//
+	// One sub-case of #2 is if the Reattach is already done, and the work grant
+	// is transferred. In that case, spawning a new worker is basically picking up
+	// that work grant off the ground and brushing the dirt off.
+	//
+	// The other sub-case of #2 is if the Reattach is _not_ done, and a sneaky
+	// goroutine _will_ drop it on the ground if we do nothing. In fact, we may
+	// even give them _our_ work grant to drop on the ground!
+	//
+	// (Actually, wait, I think that's the _only_ problem.)
 }
 
 // handleState provides atomic, finalizable tracking of a [Map] handler's
