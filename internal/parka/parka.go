3a7fdc412709155e8829dd3aa953afd488c2b133
Alex Hamlin <alex@alexhamlin.co>
2025-07-02 17:51:26 -0700

    Update the doc comment around suitability
    We have a form of task cancellation now, so the remaining gap (if it
    really is one) is cache eviction.

--- internal/parka/parka.go
+++ internal/parka/parka.go
@@ -6,8 +6,8 @@ concurrency into a single mechanism for distributing work across goroutines.
 Maps provide substantial dynamic control over concurrency limits and task
 prioritization, including at the level of individual running handlers.
 
-This version of Parka is suited for work that does not require task cancellation
-or cache eviction, such as a main package that runs a single batch-type workflow
-and exits immediately on success or error.
+This version of Parka is suited for work that does not require cache eviction,
+such as a main package running a single batch-type workflow or short-lived maps
+that are disposed of after a single use.
 */
 package parka
